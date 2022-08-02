4222083a67bc5e10f167de71b210f6dfa5285c27
mario12450 <40970808+mario12450@users.noreply.github.com>
2022-08-02 09:59:20 +0700

    Add files via upload

---
+++ jokul_config.php
@@ -0,0 +1,17 @@
+<?php 
+	
+	define("JOKUL_DEBUG", False);
+
+	// Change $JOKUL_TESTING to False for Production
+	define("JOKUL_TESTING", True);
+	define("JOKUL_URL_API_SANDBOX", "https://api-sandbox.doku.com");
+	define("JOKUL_URL_API_PRODUCTION", "https://api.doku.com");
+	define("JOKUL_URL_API_PATH_CHECKOUT", "/checkout/v1/payment");
+	define("JOKUL_URL_API", JOKUL_URL_API_SANDBOX); 
+	
+
+	define("JOKUL_CLIENT_ID", "BRN-0258-1650965087932");
+	define("JOKUL_CLIENT_SECRET", "SK-M1xy4RCcuujV63BCRVag"); 
+
+
+?>
